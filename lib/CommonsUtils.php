055576ddda16ba3eedd2ff76c896ead2f65ef556
Marck C. Guzmán <marck@marck-devs.com>
2021-09-03 13:45:54 +0200

    Added same commons utils

---
+++ lib/CommonsUtils.php
@@ -0,0 +1,16 @@
+<?php
+namespace MarckDevs\SimpleLogger;
+class CommonsUtils{
+    public static function starts_with( $haystack, $needle ) {
+        $length = strlen( $needle );
+        return substr( $haystack, 0, $length ) === $needle;
+   }
+
+   public static function ends_with( $haystack, $needle ) {
+    $length = strlen( $needle );
+    if( !$length ) {
+        return true;
+    }
+    return substr( $haystack, -$length ) === $needle;
+}
+}
